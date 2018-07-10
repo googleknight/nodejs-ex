0a7957763ebc7bc27a4f598fdaadcf83bae6a66a
Shubham Mathur <shubham.mathur@engineer.com>
2018-07-10 11:47:38 +0530

    trying something

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get('/', function (req, res) {
       if (err) {
         console.log('Error running count. Message:\n'+err);
       }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+      res.render('index.html', { pageCountMessage : process.env.default, dbInfo: dbDetails });
     });
   } else {
     res.render('index.html', { pageCountMessage : null});
